518b7667e4d71b88235aa9f884733dcab240529d
Maurice Yang <mauriceyangmy28@gmail.com>
2023-01-17 15:41:25 +0700

    Initial commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
-    daisyui: {
-        themes: [{
-            mytheme: {
-                "primary": "#0C53A6",
-                "secondary": "#D926A9",
-                "accent": "#1FB2A6",
-                "neutral": "#191D24",
-                "base-100": "#2A303C",
-                "info": "#3ABFF8",
-                "success": "#36D399",
-                "warning": "#FBBD23",
-                "error": "#F87272",
-            },
-        }]
-    },
     theme: {
         extend: {
             colors: {
